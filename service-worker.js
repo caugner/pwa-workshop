26579517aee702a5a659b172c89709435265d3b2
Claas Augner <git@caugner.de>
2018-06-07 15:38:50 +0200

    make 'Add to homescreen' work

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
     event.waitUntil(self.clients.claim());
 })
+
+self.addEventListener('fetch', function(event) {
+
+});
--- service.js
+++ service.js
@@ -1,3 +1,13 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js');
 }
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    event.userChoice.then(function (choiceResult) {
+        if (choiceResult.outcome == 'dismissed') {
+            console.log('User cancelled home screen install');
+        } else {
+            console.log('User added to home screen');
+        }
+    })
+});
