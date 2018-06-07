2c61d233022372766f9f8a56214a7d50015190b2
Claas Augner <git@caugner.de>
2018-06-07 16:19:05 +0200

    use cache

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,50 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(self.skipWaiting());
+
+    caches.open('my-cache').then(cache => {
+        cache.addAll([
+          '/',
+          'index.html',
+          'bundle.css',
+          'bundle.js',
+          'manifest.json',
+          'players.json',
+          'assets/players/ousmane-dembele.jpeg',
+          'assets/players/ngolo-kante.jpeg',
+          'assets/players/steven-nzonzi.jpeg',
+          'assets/players/djibril-sidibe.jpeg',
+          'assets/players/alphonse-areola.jpeg',
+          'assets/players/olivier-giroud.jpeg',
+          'assets/players/presnel-kimpembe.jpeg',
+          'assets/players/raphael-varane.jpeg',
+          'assets/players/lucas-hernandez.jpeg',
+          'assets/players/nabil-fekir.jpeg',
+          'assets/players/blaise-matuidi.jpeg',
+          'assets/players/florian-thauvin.jpeg',
+          'assets/players/benjamin-pavard.jpeg',
+          'assets/players/adil-rami.jpeg',
+          'assets/players/kylian-mbappe.jpeg',
+          'assets/players/benjamin-mendy.jpeg',
+          'assets/players/paul-pogba.jpeg',
+          'assets/players/corentin-tolisso.jpeg',
+          'assets/players/steve-mandanda.jpeg',
+          'assets/players/thomas-lemar.jpeg',
+          'assets/players/hugo-lloris.jpeg',
+          'assets/players/samuel-umtiti.jpeg',
+          'assets/players/antoine-griezmann.jpeg',
+        ])
+    })
 });
 
 self.addEventListener('activate', function(event) {
     event.waitUntil(self.clients.claim());
 })
 
-self.addEventListener('fetch', function(event) {
-    if (/\.jpe?g$/.test(event.request.url)) {
-        event.respondWith(
-            fetch('/assets/players/steve-mandanda.jpeg').then(function(responseFetch) {
-                return responseFetch;
-            })
-        )
-    } else {
-        event.respondWith(
-            fetch(event.request).then(function(responseFetch) {
-                return responseFetch;
-            })
-        )
-    }
-
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
 });
+
